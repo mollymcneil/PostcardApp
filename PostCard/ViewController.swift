ea258eeda98d143b2008843f0c4863ab724ac5aa
Molly McNeil <molly.mcneil@gmail.com>
2015-01-07 12:05:49 -0800

    modified text fields

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterAddressTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    
     @IBOutlet weak var mailButton: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
     
    
 
@@ -32,12 +37,28 @@ class ViewController: UIViewController {
         
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
-        messageLabel.textColor = UIColor.redColor()
+        messageLabel.textColor = UIColor.blueColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.redColor()
+        
+        addressLabel.hidden = false
+        addressLabel.text = enterAddressTextField.text
+        addressLabel.textColor = UIColor.grayColor()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        //resignFirstResponder gets rid of keyboard
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        enterAddressTextField.text = ""
+        enterAddressTextField.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent Away!", forState: UIControlState.Normal)
+        
     }
 
 }
